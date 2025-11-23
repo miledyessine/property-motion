7bc41d918260fe845debcf5eb977205e081d20bb
Miled Yessine <miled.yassine7@gmail.com>
2025-11-23 17:11:55 +0100

    enable vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./providers/AuthProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
                 <AuthProvider>{children}</AuthProvider>
+                <Analytics />
             </body>
         </html>
     );
